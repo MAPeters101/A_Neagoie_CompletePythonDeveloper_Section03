10e4343fd08816a4a07c96c765811f6475631997
Mark Peters <mapeters101@earthlink.net>
2025-04-16 11:47:34 -0400

    Work on lesson030.

---
+++ lesson030.py
@@ -0,0 +1,23 @@
+
+some_value = 5
+some_value = some_value + 2
+print(some_value)
+print()
+
+some_value = 5
+some_value += 2
+print(some_value)
+print()
+
+some_value = 5
+some_value -= 2
+print(some_value)
+print()
+
+some_value = 5
+some_value *= 2
+print(some_value)
+print()
+
+
+
