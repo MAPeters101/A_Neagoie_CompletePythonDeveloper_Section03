7a04543aa258bf75ab5234177cfdad7559dcdf43
Mark Peters <mapeters101@earthlink.net>
2025-04-16 11:39:18 -0400

    Work on lesson028.

---
+++ lesson028.py
@@ -0,0 +1,33 @@
+iq = 190
+user_iq = 191
+_user_iq = 192
+us4er_iq3 = 193
+user_IQ = 194
+#print = 195
+
+print(iq)
+print(user_iq)
+print(_user_iq)
+print(us4er_iq3)
+print(user_IQ)
+#print(print)
+
+user_age = iq/4
+print(user_age)
+a = user_age
+print(a)
+print()
+
+PI = 3.14
+print(PI)
+PI = 0
+print(PI)
+print()
+
+__hihi = 'hi'
+
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+
