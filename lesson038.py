38d087b01485c73cc85a01c92bac078682f73f80
Mark Peters <mapeters101@earthlink.net>
2025-04-17 16:00:46 -0400

    Work on lesson038.

---
+++ lesson038.py
@@ -0,0 +1,19 @@
+print(len('hellloooo'))
+
+greet = 'hellloooo'
+print(greet[:])
+print(greet[0:len(greet)])
+print()
+
+quote = 'to be or not to be'
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
+quote2 = quote.replace('be', 'me')
+
+
+print(quote)
+print(quote2)
+
+
