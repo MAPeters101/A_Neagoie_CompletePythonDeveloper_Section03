2b506fa7126df5879ee0acc9875a1696aff9d779
Mark Peters <mapeters101@earthlink.net>
2025-04-17 15:49:00 -0400

    Work on lesson036.

---
+++ lesson036.py
@@ -0,0 +1,19 @@
+selfish = 'me me me'
+
+print(selfish[0])
+print(selfish[7])
+print(selfish)
+print()
+selfish = '01234567'
+print(selfish[0:7])
+print(selfish[0:8])
+print(selfish[0:8:2])
+print(selfish[1:])
+print(selfish[:5])
+print(selfish[::1])
+print(selfish[-1])
+print(selfish[-2])
+print(selfish[-3])
+print(selfish[::-1])
+print(selfish[::-2])
+
