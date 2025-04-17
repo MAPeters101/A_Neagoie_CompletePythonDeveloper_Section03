8d1fa0cbdc5c2670ce989d67c4ad9b68a335f5ca
Mark Peters <mapeters101@earthlink.net>
2025-04-17 16:33:36 -0400

    Work on lesson040.

---
+++ lesson040.py
@@ -0,0 +1,26 @@
+name = 'Andrei Neagoie'
+age = 50
+relationship_status = 'single'
+
+relationship_status = 'it\'s complicated'
+print(relationship_status)
+print('-'*80)
+
+birth_year = input('What year were you born? ')
+current_year = input('What year is it now? ')
+print(f'You are {int(current_year)-int(birth_year)} years old.')
+print('-'*80)
+
+birth_year = input('What year were you born? ')
+age = 2025 - int(birth_year)
+print(f'Your age is: {age}')
+
+
+
+
+
+
+
+
+
+
