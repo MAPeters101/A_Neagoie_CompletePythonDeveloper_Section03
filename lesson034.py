279c12ba8f59a5c828ab44f0953f26da479e65bd
Mark Peters <mapeters101@earthlink.net>
2025-04-17 14:52:36 -0400

    Work on lesson034.

---
+++ lesson034.py
@@ -0,0 +1,17 @@
+#weather = 'It's sunny'
+weather = "It's sunny"
+weather = "It\'s \"kind of\" sunny"
+print(weather)
+
+weather = "It\\s \"kind of\" sunny"
+print(weather)
+
+weather = "\tIt\'\'s \"kind of\" sunny"
+print(weather)
+
+weather = "\tIt\'\'s \"kind of\" sunny \n hope you have a good day!"
+print(weather)
+
+
+
+
