674cb98ff349ff6fa19d787911782109d08ff8ab
Mark Peters <mapeters101@earthlink.net>
2025-04-21 15:41:58 -0400

    Work on lesson047.

---
+++ lesson047.py
@@ -0,0 +1,17 @@
+basket = ['a','b','c','d','e']
+print(basket.index('d'))
+#print(basket.index('d',0,3))
+print(basket.index('d',0,4))
+
+print('d' in basket)
+print('x' in basket)
+print('i' in 'hi my name is Ian')
+print()
+print(basket.count('d'))
+basket = ['a','b','c','d','e','d']
+print(basket.count('d'))
+
+
+
+
+
