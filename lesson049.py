79b22abf959cc76aaf8529165d3f28caf5110f11
Mark Peters <mapeters101@earthlink.net>
2025-04-21 16:09:08 -0400

    Work on lesson049.

---
+++ lesson049.py
@@ -0,0 +1,48 @@
+
+basket = ['a','x','b','c','d','e','d']
+basket.sort()
+basket.reverse()
+print(len(basket))
+print(basket)
+print(basket[::-1])
+print(basket)
+print()
+
+basket = ['a','x','b','c','d','e','d']
+basket.sort()
+basket.reverse()
+
+print(range(1,10))
+print(list(range(1,10)))
+print(list(range(10)))
+print(list(range(11)))
+print()
+
+sentence = '!'
+sentence.join(['hi', 'my', 'name', 'is', 'JOJO'])
+print(sentence)
+print()
+
+sentence = '!'
+new_sentence = sentence.join(['hi', 'my', 'name', 'is', 'JOJO'])
+print(sentence)
+print(new_sentence)
+print()
+
+sentence = ''
+new_sentence = sentence.join(['hi', 'my', 'name', 'is', 'JOJO'])
+print(sentence)
+print(new_sentence)
+print()
+
+sentence = ' '
+new_sentence = sentence.join(['hi', 'my', 'name', 'is', 'JOJO'])
+print(sentence)
+print(new_sentence)
+print()
+
+new_sentence = ' '.join(['hi', 'my', 'name', 'is', 'JOJO'])
+print(new_sentence)
+print()
+
+
